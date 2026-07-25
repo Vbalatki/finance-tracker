024c3dfc1a42392e71b432302aca7e83dafffaf5
Dmitriy <dimasidorov734@gmail.com>
2026-07-25 23:50:03 +0300

    fixed csrf token bug

--- src/main/java/com/finance/finance_tracker/controller/AccessDeniedController.java
+++ src/main/java/com/finance/finance_tracker/controller/AccessDeniedController.java
@@ -1,12 +1,13 @@
 package com.finance.finance_tracker.controller;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 public class AccessDeniedController {
 
-    @GetMapping("/access-denied")
+    @RequestMapping(value = "/access-denied", method = {RequestMethod.GET, RequestMethod.POST})
     public String accessDeniedPage() {
         return "error/access-denied";
     }
