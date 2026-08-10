6a875f0abd640654522231d91a7e9f18911b8653
Dmitriy <dimasidorov734@gmail.com>
2026-08-10 13:43:45 +0300

    lower&upper Rigester

--- src/main/java/com/finance/finance_tracker/dto/RoleDto.java
+++ src/main/java/com/finance/finance_tracker/dto/RoleDto.java
@@ -14,7 +14,6 @@ public class RoleDto {
     private Long id;
 
     @NotBlank(message = "Название роли не может быть пустым")
-    @Pattern(regexp = "^[A-Z_]+$", message = "Название роли должно содержать только заглавные буквы и подчёркивания")
     @Size(min = 2, max = 50, message = "Название роли должно быть от 2 до 50 символов")
     private String name;
 
